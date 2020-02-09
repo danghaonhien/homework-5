d4842eee7a6276be75798b8b6873a72db04f5851
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-09 14:52:22 -0800

    add click button

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+
 console.log(moment())
 var day = moment();
 $("#realTime").text(day)
+// let $yourText = $(".yourText").val();
+$("#do").on("click", function(){
+$("input[type='text']").each(function(){
+    var id = $(this).attr('id');
+    var value = $(this).val();
+    localStorage.setItem(id, value, moment())
+
+}) 
+})
+$("#dont").on("click", function(){
+    $("#yourText").val("")
+    $("input[type='text']").each(function(){
+        var id = $(this).attr('id');
+       var value= localStorage.removeItem(id)
+        $(this).val(value)
+    })   
+})
+
+
 
