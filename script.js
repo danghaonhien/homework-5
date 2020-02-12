46401fd7b747f12fc86a6bfaae93b65874e22b0c
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-11 22:52:27 -0800

    add storage

--- script.js
+++ script.js
@@ -10,22 +10,25 @@ var data = JSON.parse(localStorage.getItem("todoData"));
 data =data || {};
 $("#addBtn").on("click", function(e){
     e.preventDefault();
+    
+    let $time = $("#time").text(time);
    
-    let newTask = $(".myInput").length+1;
-    $("#myList").append('<div class="myInput">  <div class="ui equal width grid">  <div class="column">   <div class="ui segment" id="time" id="timeValue"></div></div> <div class="eight wide column"><div class="ui fluid icon input"> <input type="text" id="yourText" placeholder="Plan your Tackle...."/><i class="hand rock outline icon" class="icon"></i></div></div>   <div class="column"><div class="ui buttons"><button class="ui button" id="dont">Dont</button><div class="or"></div><button class="ui secondary button" id="do">Do</button></div></div>')
+    $("#myList").append('<div class="myInput">  <div class="ui equal width grid">  <div class="column">   <div class="ui segment" id="time" class="time"></div></div> <div class="eight wide column"><div class="ui fluid icon input"> <input type="text" id="yourText" placeholder="Plan your Tackle...."/><i class="hand rock outline icon" class="icon"></i></div></div>   <div class="column"><div class="ui buttons"><button class="ui button" id="dont">Dont</button><div class="or"></div><button class="ui secondary button" id="do">Do</button></div></div>')
+
 })
 // let $yourText = $(".yourText").val();
 $(document).on('click','#do', function(){
 $("input[type='text']").each(function(){
-    var $id = $(this).attr('class');
+    // var $id = $(this).attr('class');
     var value = $(this).val();
-    id = new Date().getTime();
+   var id = new Date().getTime();
     $("#yourText").css("border-color", "green")
     tempData = {
         id : id,
+        text:value,
         hour:time,
         date: day,
-        text:value
+        // input:$id
     };
     // Saving element in local storage
     data[id] = tempData;
@@ -37,9 +40,8 @@ $("input[type='text']").each(function(){
 $(document).on('click','#dont', function(){
     $(this).closest('.myInput').remove();
     $("input[type='text']").each(function(){
-    var id = $(this).attr('id');
-    var value= localStorage.removeItem(id)
-    $(this).val(value)       
+        data = {};
+        localStorage.setItem("todoData", JSON.stringify(data));     
 })
  })
 })
