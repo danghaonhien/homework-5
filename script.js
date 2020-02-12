eb1e182e4dbc5d47ec4323c14164dab44dbd0d9f
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-11 19:44:03 -0800

    add new tackle

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+    
 console.log(moment())
 var day = moment();
 $("#realTime").text(day)
@@ -11,29 +12,38 @@ $("#addBtn").on("click", function(e){
     // let newDiv = $("<div>")
     // newDiv.append(myList)
     // $("#newTask").append($(newDiv))
-    $(myList).after($("#yourText"))
+    let newTask = $(".myInput").length+1;
+    $("#myList").append('<div class="myInput">  <div class="ui equal width grid">  <div class="column">   <div class="ui segment" id="time" id="timeValue"></div></div> <div class="eight wide column"><div class="ui fluid icon input"> <input type="text" id="yourText" placeholder="Plan your Tackle...."/><i class="hand rock outline icon" class="icon"></i></div></div>   <div class="column"><div class="ui buttons"><button class="ui button" id="dont">Dont</button><div class="or"></div><button class="ui secondary button" id="do">Do</button></div></div>')
 })
 
 // let $yourText = $(".yourText").val();
 $("#do").on("click", function(){
 $("input[type='text']").each(function(){
-    var id = $(this).attr('id');
+    var id = $(this).attr('class');
     var value = $(this).val();
-    $("#yourText").css("border-color", "green")
-    localStorage.setItem(id, value)
-    
+    $(".yourText").css("border-color", "green")
+    localStorage.setItem(id, value) 
 }) 
 
 })
-$("#dont").on("click", function(){
-    $("#yourText").val("")
+$(document).on('click','#dont', function(){
+    $(this).closest('.myInput').remove();
     $("input[type='text']").each(function(){
-        $("#yourText").css("border-color", "red")
-        var id = $(this).attr('id');
-       var value= localStorage.removeItem(id)
-        $(this).val(value)
-    })   
+    $("#yourText").val("")
+    var id = $(this).attr('id');
+    var value= localStorage.removeItem(id)
+    $(this).val(value)
+    
+       
 })
+ })
+// $("#dont").on("click", function(){
+ 
+    
+//         $("#yourText").css("border-color", "red")
+//        
+   
+// })
 })
 
 
