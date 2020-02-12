b867df8cee12b91d477bc3b3c2012ac21ccbad8a
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-11 22:16:30 -0800

    add time and house to storage

--- script.js
+++ script.js
@@ -2,48 +2,46 @@
 $(document).ready(function(){
     
 console.log(moment())
-var day = moment();
+var day = moment().format('MMM Do YY');
 $("#realTime").text(day)
-let time = 7;
-
-let myList = $("#myList")
+let time = moment().format("hh:mm a");
+$("#time").text(time);
+var data = JSON.parse(localStorage.getItem("todoData"));
+data =data || {};
 $("#addBtn").on("click", function(e){
     e.preventDefault();
-    // let newDiv = $("<div>")
-    // newDiv.append(myList)
-    // $("#newTask").append($(newDiv))
+   
     let newTask = $(".myInput").length+1;
     $("#myList").append('<div class="myInput">  <div class="ui equal width grid">  <div class="column">   <div class="ui segment" id="time" id="timeValue"></div></div> <div class="eight wide column"><div class="ui fluid icon input"> <input type="text" id="yourText" placeholder="Plan your Tackle...."/><i class="hand rock outline icon" class="icon"></i></div></div>   <div class="column"><div class="ui buttons"><button class="ui button" id="dont">Dont</button><div class="or"></div><button class="ui secondary button" id="do">Do</button></div></div>')
 })
-
 // let $yourText = $(".yourText").val();
-$("#do").on("click", function(){
+$(document).on('click','#do', function(){
 $("input[type='text']").each(function(){
-    var id = $(this).attr('class');
+    var $id = $(this).attr('class');
     var value = $(this).val();
-    $(".yourText").css("border-color", "green")
-    localStorage.setItem(id, value) 
+    id = new Date().getTime();
+    $("#yourText").css("border-color", "green")
+    tempData = {
+        id : id,
+        hour:time,
+        date: day,
+        text:value
+    };
+    // Saving element in local storage
+    data[id] = tempData;
+    localStorage.setItem("todoData", JSON.stringify(data));
+  
 }) 
 
 })
 $(document).on('click','#dont', function(){
     $(this).closest('.myInput').remove();
     $("input[type='text']").each(function(){
-    $("#yourText").val("")
     var id = $(this).attr('id');
     var value= localStorage.removeItem(id)
-    $(this).val(value)
-    
-       
+    $(this).val(value)       
 })
  })
-// $("#dont").on("click", function(){
- 
-    
-//         $("#yourText").css("border-color", "red")
-//        
-   
-// })
 })
 
 
