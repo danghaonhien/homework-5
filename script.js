bcdcb403941fdc412256b07845d73d23b39ed977
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-22 22:28:47 -0800

    change js

--- script.js
+++ script.js
@@ -1,54 +1,96 @@
 
 $(document).ready(function(){
-var todoInput = $("#todo-text");
-var todoForm = $("#todo-form");
-var todoList = $("#todo-list");
-var todoCountSpan = $("#todo-count");
-var todos = [];
+
 
 console.log(moment())
 var day = moment().format('MMM Do YY');
 $("#realTime").text(day)
 let time = moment().format("hh:mm a");
 $("#time").text(time);
-var data = JSON.parse(localStorage.getItem("todoData"));
-data =data || {};
-$("#addBtn").on("click", function(e){
-    e.preventDefault();
-    
-    $("#time").text(time);
-   
-    $("#myList").append('<div class="myInput">  <div class="ui equal width grid">  <div class="column">   <div class="ui segment" id="time" class="time"></div></div> <div class="eight wide column"><div class="ui fluid icon input"> <input type="text" id="yourText" placeholder="Plan your Tackle...."/><i class="hand rock outline icon" class="icon"></i></div></div>   <div class="column"><div class="ui buttons"><button class="ui button" id="dont">Dont</button><div class="or"></div><button class="ui secondary button" id="do">Do</button></div></div>')
+
 
 })
 
-$(document).on('click','#do', function(){
-$("input[type='text']").each(function(){
-    // var $id = $(this).attr('class');
-    var value = $(this).val();
-   var id = new Date().getTime();
-    $("#yourText").css("border-color", "green")
-    tempData = {
-        id : id,
-        text:value,
-        hour:time,
-        date: day,
-        // input:$id
-    };
-    // Saving element in local storage
-    data[id] = tempData;
-    localStorage.setItem("todoData", JSON.stringify(data));
-  
-}) 
+var todoInput = $("#todo-text");
+var todoForm = $("#todo-form");
+var todoList = $("#todo-list");
+var todoCountSpan = $("#todo-count");
+var todos = [];
+init();
 
-})
-$(document).on('click','#dont', function(){
-    $(this).closest('.myInput').remove();
-    $("input[type='text']").each(function(){
-        data = {};
-        localStorage.setItem("todoData", JSON.stringify(data));     
-})
- })
-})
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.html("");
+  todoCountSpan.text(todos.length);
 
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = $("<li>");
+    li.text(todo);
+    li.attr("data-index", i);
+
+    var button = $("<button>");
+    button.text("Done")
+
+    li.append(button);
+    todoList.append(li);
+  }
+}
+
+function init() {
+  // Get stored todos from localStorage
+  // Parsing the JSON string to an object
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedTodos !== null) {
+    todos = storedTodos;
+  }
+
+  // Render todos to the DOM
+  renderTodos();
+}
+
+function storeTodos() {
+  // Stringify and set "todos" key in localStorage to todos array
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// When form is submitted...
+todoForm.on("submit", function(event) {
+  event.preventDefault();
+
+  var todoText = todoInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (todoText === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  todos.push(todoText);
+  todoInput.value = "";
+
+  // Store updated todos in localStorage, re-render the list
+  storeTodos();
+  renderTodos();
+});
+
+// When a element inside of the todoList is clicked...
+todoList.on("click", function(event) {
+  var element = event.target;
+
+  // If that element is a button...
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    todos.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    storeTodos();
+    renderTodos();
+  }
+});
 
